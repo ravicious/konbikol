811cca6ddc3d4b8080a6baeed6825faa77674100
Rafał Cieślak <ravicious@gmail.com>
2019-11-01 18:06:29 +0100

    Parse text from selected PDF file

--- init.js
+++ init.js
@@ -1,3 +1,41 @@
+'use strict';
+
 var app = Elm.Main.init({
   node: document.getElementById('app-container')
 })
+
+var pdfjsLib = window['pdfjs-dist/build/pdf']
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.2.228/pdf.worker.min.js'
+
+app.ports.parsePdf.subscribe(function(file) {
+  getPdfTextFromFile(file).then(console.log, function(error) {
+    if (error.name == 'InvalidPDFException') {
+      // TODO: Tell Elm the file is invalid.
+      console.error('Invalid PDF!')
+    } else {
+      console.error(error)
+    }
+  })
+})
+
+function getPdfTextFromFile(file) {
+  return new Promise(function(resolve, reject) {
+    var fileReader = new FileReader()
+
+    fileReader.onload = function() {
+      var typedArray = this.result
+      var pdf = pdfjsLib.getDocument(typedArray).promise
+
+      resolve(pdf.then(function(pdf) {
+        return pdf.getPage(1)
+      }).then(function(page) {
+        return page.getTextContent()
+      }).then(function(textContent) {
+        return textContent.items.map(function (s) { return s.str }).join("\n")
+      })
+      )
+    }
+
+    fileReader.readAsArrayBuffer(file)
+  })
+}
--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
+import File exposing (File)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as Decode
+import Json.Encode as Encode
 
 
 main =
@@ -17,8 +22,11 @@ type alias Model =
     ()
 
 
-type alias Msg =
-    ()
+type Msg
+    = GotFile Decode.Value
+
+
+port parsePdf : Encode.Value -> Cmd msg
 
 
 init : () -> ( Model, Cmd Msg )
@@ -28,9 +36,32 @@ init _ =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotFile file ->
+            ( model, parsePdf file )
+
+
+fileDecoder : Decode.Decoder Decode.Value
+fileDecoder =
+    Decode.at [ "target", "files" ] (Decode.list Decode.value)
+        |> Decode.andThen
+            (\files ->
+                case List.head files of
+                    Just file ->
+                        Decode.succeed file
+
+                    Nothing ->
+                        Decode.fail "No files found under target.files of change event of an input[type=\"file\"]"
+            )
 
 
 view : Model -> Html Msg
 view model =
-    text "Hello, World!"
+    div []
+        [ input
+            [ type_ "file"
+            , accept "application/pdf"
+            , on "change" (Decode.map GotFile fileDecoder)
+            ]
+            []
+        ]
